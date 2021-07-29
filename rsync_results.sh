e4e7aa6cb3a679c8190c7aa22fc5868d6cd71ac3
javieryu <javiercyu@gmail.com>
2021-07-29 13:01:26 -0700

    rsync script touchup

--- rsync_results.sh
+++ rsync_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-LOCALDIR=/home/javieryu/workspace/nn_distributed_training
+LOCALDIR=/home/javier/workspace/nn_distributed_training
 REMOTE=javieryu@bespin.stanford.edu
-REMOTEDIR=~/workspace/nn_distributed_training/results
-OPTS="--dry-run -avzhe ssh --progress"
+REMOTEDIR=/home/javieryu/workspace/nn_distributed_training/results
+OPTS="-avzhe ssh --progress"
 rsync $OPTS $REMOTE:$REMOTEDIR $LOCALDIR
