482cd02a277ffaaebc973361c8220f3def259fb0
Joe-Vincent <joevincentkc@gmail.com>
2021-08-26 15:10:40 -0500

    process first run

--- RL/plot_reward.py
+++ RL/plot_reward.py
@@ -2,15 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("dark_background")
 
-avg_ep_rews = np.load('avg_ep_rews.npy')
-timesteps = np.load('timesteps.npy')
+times = np.load('trained/timesteps_1.npy')
+rewards = np.load('trained/avg_ep_rews_1.npy')
 
 fig, ax0 = plt.subplots(figsize=(20, 8))
-
-ax0.plot(timesteps, avg_ep_rews, c="indianred")
+ax0.plot(times, rewards, c="indianred")
 # ax0.fill_between(np.arange(vlb.shape[0]), torch.amax(vlb, dim=1), torch.amin(vlb, dim=1), color="indianred", alpha=0.5)
 ax0.set_title("RL Training Over Time")
 ax0.set_xlabel("Timestep")
 ax0.set_ylabel("Average Episode Reward")
 
 plt.show()
+
+
