31073d18fb21107e0caf7811e2b9f609801a1a53
Joe-Vincent <joevincentkc@gmail.com>
2021-09-05 20:50:07 -0500

    clean up saved stats

--- RL/plot_reward.py
+++ RL/plot_reward.py
@@ -3,23 +3,20 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 16})
 
 # Load Centralized
-times_cent = np.load('trained/timesteps_2.npy')
-rewards1 = np.load('trained/avg_ep_rews_1.npy')
-rewards2 = np.load('trained/avg_ep_rews_2.npy')
+times_cent = np.load('trained/timesteps_3.npy')
 rewards3 = np.load('trained/avg_ep_rews_3.npy')
 rewards4 = np.load('trained/avg_ep_rews_4.npy')
-rewards_arr_cent = np.vstack((rewards1, rewards2, rewards3, rewards4))
 
-times_long = np.hstack((times_cent, times_cent[-1] + np.load('trained/timesteps_4_2.npy')))
-rewards_long = np.hstack((rewards4, np.load('trained/avg_ep_rews_4_2.npy')))
+rewards_arr_cent = np.vstack((rewards3, rewards4))
+
 
 # Load CADMM
-times_cadmm = np.load('dist_rl/trained/timesteps_1.npy')
-rewards1 = np.load('dist_rl/trained/avg_ep_rews_1.npy')
-rewards2 = np.load('dist_rl/trained/avg_ep_rews_2.npy')
-rewards3 = np.load('dist_rl/trained/avg_ep_rews_3.npy')
-rewards4 = np.load('dist_rl/trained/avg_ep_rews_4.npy')
-rewards_arr_cadmm = np.vstack((rewards1, rewards2, rewards3, rewards4))
+# times_cadmm = np.load('dist_rl/trained/timesteps_1.npy')
+# rewards1 = np.load('dist_rl/trained/avg_ep_rews_1.npy')
+# rewards2 = np.load('dist_rl/trained/avg_ep_rews_2.npy')
+# rewards3 = np.load('dist_rl/trained/avg_ep_rews_3.npy')
+# rewards4 = np.load('dist_rl/trained/avg_ep_rews_4.npy')
+# rewards_arr_cadmm = np.vstack((rewards1, rewards2, rewards3, rewards4))
 
 times_long_cadmm = np.load('dist_rl/trained/timesteps_long.npy')
 rewards_long_cadmm = np.load('dist_rl/trained/avg_ep_rews_long.npy')
@@ -44,16 +41,11 @@ ax0.plot(times_cent, np.mean(rewards_arr_cent, axis=0), c=cent_color, label="Cen
 ax0.fill_between(times_cent, np.amax(rewards_arr_cent, axis=0), np.amin(rewards_arr_cent, axis=0), color=cent_color, alpha=0.5)
 
 # CADMM
-ax0.plot(times_cadmm, np.mean(rewards_arr_cadmm, axis=0), c=cadmm_color, label="CADMM")
-ax0.fill_between(times_cadmm, np.amax(rewards_arr_cadmm, axis=0), np.amin(rewards_arr_cadmm, axis=0), color=cadmm_color, alpha=0.5)
-
-# Long Centralized
-ax0.plot(times_long, rewards_long, c=cent_color, label="Long Centralized")
+# ax0.plot(times_cadmm, np.mean(rewards_arr_cadmm, axis=0), c=cadmm_color, label="CADMM")
+# ax0.fill_between(times_cadmm, np.amax(rewards_arr_cadmm, axis=0), np.amin(rewards_arr_cadmm, axis=0), color=cadmm_color, alpha=0.5)
 
 # Long CADMM
 ax0.plot(times_long_cadmm, rewards_long_cadmm, c=cadmm_color, label="Long CADMM")
-
-
 
 # DSGT
 
