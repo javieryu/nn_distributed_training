48ac84ce7fe8df6c7bdc1bcbadf1639b1f1b96df
Javier Yu <javiercyu@gmail.com>
2021-10-12 10:21:49 -0700

    remove rsync

--- rsync_results.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-LOCALDIR=/home/javier/workspace/nn_distributed_training
-REMOTE=javieryu@bespin.stanford.edu
-REMOTEDIR=/home/javieryu/workspace/nn_distributed_training/results
-OPTS="-avzhe ssh --progress"
-rsync $OPTS $REMOTE:$REMOTEDIR $LOCALDIR
