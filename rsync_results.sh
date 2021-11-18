1382401e61bb65d6ddb14ca3868dd365349dbbb1
javieryu <javiercyu@gmail.com>
2021-11-18 11:58:48 -0800

    rsync homer house

--- rsync_results.sh
+++ rsync_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOCALDIR=/home/javier/workspace/nn_distributed_training
-REMOTE=javieryu@bespin.stanford.edu
-REMOTEDIR=/home/javieryu/workspace/nn_distributed_training/results
+REMOTE=homer
+REMOTEDIR=/home/javier/workspace/nn_distributed_training/results_scaling
 OPTS="-avzhe ssh --progress"
 rsync $OPTS $REMOTE:$REMOTEDIR $LOCALDIR
